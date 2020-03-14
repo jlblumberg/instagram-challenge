9527f9a3c18639d9d6609ec9dd6583199895b803
Josh Blumberg <jlblumberg@gmail.com>
2020-03-14 14:58:21 +0000

    Add username to devise sign up stage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_134133) do
+ActiveRecord::Schema.define(version: 2020_03_14_143902) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -25,8 +25,10 @@ ActiveRecord::Schema.define(version: 2020_03_14_134133) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
