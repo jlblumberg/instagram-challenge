88eee4bb77787bb0232ccf6da2c5cdc02cac7d2a
Josh Blumberg <jlblumberg@gmail.com>
2020-03-15 12:49:54 +0000

    Add image to post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_15_113844) do
+ActiveRecord::Schema.define(version: 2020_03_15_124913) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "message"
     t.integer "user_id"
+    t.string "image"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
